d61a598f979435a2bbe1c06f4def4ae758986778
Patrick Boettcher <p@yai.se>
2021-11-29 11:25:10 +0100

    ui: create basic user-interface (#16)
    fix #2

---
+++ example/tscat-gui.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+from PySide2 import QtWidgets
+from PySide2 import QtCore
+from PySide2 import QtGui
+
+import sys
+
+from tscat_gui import TSCatGUI
+
+if __name__ == "__main__":
+    # QtWidgets.QApplication.setDesktopSettingsAware(False)  # defaulting to light mode
+
+    app = QtWidgets.QApplication(sys.argv)
+
+    main = QtWidgets.QMainWindow()
+
+    w = TSCatGUI(main)
+
+    main.setCentralWidget(w)
+
+    #     styles = """
+    # QTreeView::!active { selection-background-color: gray;}
+    # """
+    #     main.setStyleSheet(styles)
+
+    main.show()
+
+    sys.exit(app.exec_())
--- tscat_gui/__init__.py
+++ tscat_gui/__init__.py
@@ -3,3 +3,41 @@
 __author__ = """Patrick Boettcher"""
 __email__ = 'p@yai.se'
 __version__ = '0.1.0'
+
+from PySide2 import QtWidgets
+from PySide2 import QtCore
+from PySide2 import QtGui
+
+
+class TSCatGUI(QtWidgets.QWidget):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
+        events = QtWidgets.QTableView()
+        events.setMinimumSize(1000, 500)
+
+        details = QtWidgets.QWidget(self)
+        details.setMinimumHeight(400)
+        details_layout = QtWidgets.QGridLayout(details)
+        details_layout.setMargin(0)
+
+        splitter_right = QtWidgets.QSplitter(QtCore.Qt.Vertical, self)
+        splitter_right.addWidget(events)
+        splitter_right.addWidget(details)
+
+        catalogues = QtWidgets.QTreeView()
+        catalogues.setMinimumSize(300, 900)
+
+        splitter = QtWidgets.QSplitter(QtCore.Qt.Horizontal, self)
+        splitter.addWidget(catalogues)
+        splitter.addWidget(splitter_right)
+
+        layout = QtWidgets.QVBoxLayout()
+        layout.setContentsMargins(0, 0, 0, 0)
+
+        toolbar = QtWidgets.QToolBar()
+        toolbar.addAction(QtWidgets.QAction(QtGui.QIcon.fromTheme('folder-new'), "Create Catalogue", self))
+
+        layout.addWidget(toolbar)
+        layout.addWidget(splitter)
+        self.setLayout(layout)
